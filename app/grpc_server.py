883f7550312446fa95228990d14b0aacb36da88b
Qingwei Zhang <qzhang.canada@gmail.com>
2025-07-05 01:02:34 -0400

    add

--- app/grpc_server.py
+++ app/grpc_server.py
@@ -1,27 +1,25 @@
 import asyncio
 from concurrent import futures
+
 import grpc
 from app.protos import signal_pb2_grpc, signal_pb2
+from app.services.aggregator import get_signal
 
-from services.aggregator import get_signal
-
-class SignalAggregator(signal_pb2_grpc.SignalAggregatorServicer):
+class SignalService(signal_pb2_grpc.SignalAggregatorServicer):
     async def GetSignal(self, request, context):
-        data = get_signal(request.symbol)
-        return signal_pb2.SignalResponse(
-            symbol=data["symbol"],
-            price=data["price"],
-            volume=data["volume"],
-            timestamp=data["timestamp"]
-        )
+        symbol = request.symbol
+        signal_value = await get_signal(symbol)
+        return signal_pb2.SignalReply(symbol=symbol, signal=signal_value)
 
-def serve():
+
+async def serve():
     server = grpc.aio.server()
-    signal_pb2_grpc.add_SignalAggregatorServicer_to_server(SignalAggregator(), server)
+    signal_pb2_grpc.add_SignalAggregatorServicer_to_server(SignalService(), server)
     server.add_insecure_port('[::]:50051')
+    await server.start()
     print("gRPC server listening on port 50051")
-    asyncio.get_event_loop().run_until_complete(server.start())
-    asyncio.get_event_loop().run_until_complete(server.wait_for_termination())
+    await server.wait_for_termination()
+
 
 if __name__ == "__main__":
-    serve()
+    asyncio.run(serve())
